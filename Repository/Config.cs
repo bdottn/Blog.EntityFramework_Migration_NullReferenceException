b961723493e52c91c99be811a76205e164a49af9
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-08 14:04:06 +0800

    建立 Config
    1.　加入組件參考：System.Configuration
    
    2.　加入屬性：LabConnectionString
    　2.1　使用 ConfigurationManager 抓取 App.config 節點名稱為 Lab 的資料庫連線字串

---
+++ Repository/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace Repository
+{
+    public sealed class Config
+    {
+        public static string LabConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["Lab"].ConnectionString;
+            }
+        }
+    }
+}
