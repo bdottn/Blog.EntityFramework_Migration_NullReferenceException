9ce9382484663ffac08305707ef2cfdca026d3ae
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-08 14:06:05 +0800

    建立 ConfigTests 單元測試
    1.　建立 App.config
    　1.1　加入 connectionStrings 節點
    　　1.1.1　加入連線字串節點：Lab
    
    2.　加入測試案例：LabConnectionStringTest_預期回傳連線字串

---
+++ RepositoryUnitTest/ConfigTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Repository.UnitTest
+{
+    [TestClass]
+    public sealed class ConfigTests
+    {
+        [TestMethod]
+        public void LabConnectionStringTest_預期回傳連線字串()
+        {
+            var expected = @"Server=.;Database=Lab;Integrated Security=True;";
+
+            var actual = Config.LabConnectionString;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
